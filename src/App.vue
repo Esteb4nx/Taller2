cc090deb7756b43726ff8fb1f326133d436d9912
Esteban Esparza <e.esparza02@ufromail.cl>
2022-06-17 19:55:48 -0400

    Se crea NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar></NavBar>
+  <div class="mt-5 d-flex justify-content-center flex-column align-items-center">
+    <GatoVue></GatoVue>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+import GatoVue from '@/components/GatoVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,GatoVue
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid justify-content-center">
+            <a class="navbar-brand" href="#">
+                <img src="@/assets/pngegg.png" alt="" height="48"
+                    class="d-inline-block align-text-center">
+                Rick & Morty Tic tac toe
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
