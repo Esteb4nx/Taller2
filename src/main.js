99d6d59d8254371c64ef30dbaa39c01b52649dbf
Esteban Esparza <e.esparza02@ufromail.cl>
2022-06-17 19:30:18 -0400

    Se importa pinia y bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'bootstrap'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
